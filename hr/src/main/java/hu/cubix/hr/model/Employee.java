bec088a961d9faf727748871f1ecfbf74eff46e7
Fehér Zsuzsanna <zsuzsanna.feher@rrsoftware.hu>
2023-09-28 16:35:58 +0200

    Add Employee class

---
+++ hr/src/main/java/hu/cubix/hr/model/Employee.java
@@ -0,0 +1,50 @@
+package hu.cubix.hr.model;
+
+import java.time.LocalDateTime;
+
+public class Employee {
+
+	private long id;
+	private String job;
+	private int salary;
+	private LocalDateTime entryDate;
+
+	public Employee(long id, String job, int salary, LocalDateTime entryDate) {
+		this.id = id;
+		this.job = job;
+		this.salary = salary;
+		this.entryDate = entryDate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public LocalDateTime getEntryDate() {
+		return entryDate;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public void setEntryDate(LocalDateTime entryDate) {
+		this.entryDate = entryDate;
+	}
+}
